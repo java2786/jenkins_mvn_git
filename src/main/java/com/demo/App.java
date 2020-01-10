b8f44663223b38b9e453cfadf59a58bebb758497
Arun Kumar <java.kumar.arun@gmail.com>
2020-01-10 11:02:41 +0530

    compilation error gone

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -6,12 +6,8 @@ package com.demo;
  */
 public class App 
 {
-	int a;
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        String name = null;
-        System.out.println(name.charAt(0));        
-        System.out.println(a);
     }
 }
