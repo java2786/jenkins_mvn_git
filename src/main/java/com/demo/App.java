1e273a27e834e54bae2f2717c9faa7bd34a0e17f
Arun Kumar <java.kumar.arun@gmail.com>
2020-01-10 10:56:37 +0530

    first commit

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	int a;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String name = null;
+        System.out.println(name.charAt(0));        
+        System.out.println(a);
+    }
+}
